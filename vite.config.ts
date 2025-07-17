7e7cb4f97a3dda2b02840090264bffd3260494c8
Ashraf M. Jambi <15023063@saudiags.com>
2025-07-18 02:34:42 +0300

    feat: وصف موجز ومفيد للتحديثات

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
+    base: '/',
     plugins: [react()],
     define: {
       // Vite will replace `process.env.API_KEY` with the value of the API_KEY
